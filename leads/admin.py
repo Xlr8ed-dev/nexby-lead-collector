f8bdd6bc9dc9413fc08a0d23583e740dc882324b
omkar-atishay <omkar.surve@atishay.com>
2025-12-01 15:26:47 +0530

    Add ContactUs model to admin with custom display
    Registered the ContactUs model in Django admin with a custom ModelAdmin. Added list_display and search_fields for improved admin usability.

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from leads.models import ContactUs
+
+
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = (
+        "full_name",
+        "company_email",
+        "mobile_number",
+        "plan_details",
+        "country_code",
+        "is_event_management",
+        "lead_capture_method",
+        "annual_exhibitions",
+        "created_at",
+    )
+    search_fields = (
+        "full_name",
+        "company_email",
+        "mobile_number",
+        "plan_details",
+        "country_code",
+        "is_event_management",
+        "lead_capture_method",
+        "annual_exhibitions",
+        "created_at",
+    )
+
+
+admin.site.register(ContactUs, ContactUsAdmin)
